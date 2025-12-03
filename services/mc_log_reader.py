dd9942c21935385874a177abb8810ef08dc4f7b3
Stepan Potapenko <sapotapenko@edu.hse.ru>
2025-12-04 01:41:52 +0300

    log_reader

---
+++ services/mc_log_reader.py
@@ -0,0 +1,30 @@
+import subprocess
+import re
+import json
+import sys
+
+CHAT_REGEX = re.compile(
+    r"\[(?P<time>\d{2}:\d{2}:\d{2}) INFO]: <(?P<player>[^>]+)> (?P<message>.*)"
+)
+
+process = subprocess.Popen(
+    ["java", "-Xms2G", "-Xmx2G", "-jar", "paper-1.21.10-115.jar", "--nogui"],
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE,
+    text=True,
+    bufsize=1
+)
+
+print("Сервер запущен. Читаю чат...\n")
+sys.stdout.flush()
+
+for line in process.stdout:
+    line = line.strip()
+    m = CHAT_REGEX.search(line)
+    if m:
+        event = {
+            "time": m.group("time"),
+            "player": m.group("player"),
+            "message": m.group("message")
+        }
+        print(json.dumps(event, ensure_ascii=False))
