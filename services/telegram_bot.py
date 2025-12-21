c102f1896e2dd03ae977f5a711697fadf7721d04
unt0u4able <a.m.darchiev@gmail.com>
2025-12-21 19:35:35 +0300

    try stat

--- services/telegram_bot.py
+++ services/telegram_bot.py
@@ -3,10 +3,12 @@ from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import Command
 from config import TG_TOKEN
 from services import mc_rcon
+from typing import Any
 
 bot = Bot(token=TG_TOKEN)
 dp = Dispatcher()
 grps: set[int] = set()
+users: dict[str, dict[Any, Any]] = dict()
 
 
 @dp.message(Command("start"))
@@ -27,6 +29,12 @@ async def group_message(message: types.Message):
         return
     try:
         username = message.from_user.username
+        if not(username is None):
+            if (username in users):
+                users["username"]["count"] += 1
+            else:
+                users["username"]["count"] = 1
+                users["username"]["mnname"] = " Неизвесетен"
         if username is None:
             username = message.from_user.full_name
         text = message.text or ""
@@ -39,3 +47,27 @@ async def group_message(message: types.Message):
                 i += 1
     except Exception as e:
         print(f"Ошибка отправки в MC: {e}")
+
+
+@dp.message(Command("stat"))
+async def stat(message: types.Message):
+    user = message.from_user.username
+    if user is None:
+        message.answer(f"Нет информации о данном пользователе")
+    if user not in users:
+        await message.answer(f"Нет информации о данном пользователе")
+    else:
+        cntintg = users[user]["count"]
+        mnname = users[user]["mnname"]
+        await message.answer(f"Ник пользователя в маинкрафте:{mnname}\nКоличество отправленных сообщений на сервер: {cntintg}")
+
+
+@dp.message(Command("connect"))
+async def connect(message: types.Message):
+    user = message.from_user.username
+    if user is None:
+        await message.answer(f"Нет информации о данном пользователе")
+    text = message.text[8:]
+    users[user]["mnname"] = text
+    await message.answer(f"Изменения успешны!")
+    
